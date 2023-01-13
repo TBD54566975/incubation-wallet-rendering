867935909639d5c6d08a5646677692595b258ab0
Andor Kesselman <andor@henosisknot.com>
2023-01-13 19:09:53 +0530

    wip: examples

--- playground/playground.js
+++ playground/playground.js
@@ -3,4 +3,32 @@ $("document").ready(function () {
   var editor = ace.edit(aceId);
   editor.session.setMode("ace/mode/json");
   editor.setTheme("ace/theme/twilight");
+
+  var selections = [
+    {
+      name: "Entity Styles",
+      id: "entity-styles",
+      path: "entity-styles.json",
+    },
+    {
+      name: "Data Mapping Object With Text",
+      id: "dmo-with-text",
+      path: "dmo-with-text.json",
+    },
+    {
+      name: "Data Mapping Object With Path",
+      id: "dmo-with-path",
+      path: "dmo-with-path.json",
+    },
+    {
+      name: "Labelled Data Mapping Object With Path",
+      id: "dmo-with-path",
+      path: "dmo-with-path.json",
+    },
+    {
+      name: "Labelled Data Mapping Object With Text",
+      id: "ldmo-with-text",
+      path: "dmo-with-text.json",
+    },
+  ];
 });
