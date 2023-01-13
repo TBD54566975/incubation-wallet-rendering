b689741da84a3103479d91dd60a5c4d35a705668
Andor Kesselman <andor@henosisknot.com>
2023-01-13 19:50:27 +0530

    playground is working

--- playground/playground.js
+++ playground/playground.js
@@ -1,8 +1,12 @@
 $("document").ready(function () {
-  var aceId = $("#ace-editor").attr("id");
-  var editor = ace.edit(aceId);
-  editor.session.setMode("ace/mode/json");
-  editor.setTheme("ace/theme/twilight");
+  var exampleSelector = $("#example-selection");
+
+  var createEditor = function () {
+    var aceId = $("#ace-editor").attr("id");
+    var editor = ace.edit(aceId);
+    editor.session.setMode("ace/mode/json");
+    editor.setTheme("ace/theme/twilight");
+  };
 
   var selections = [
     {
@@ -31,4 +35,50 @@ $("document").ready(function () {
       path: "dmo-with-text.json",
     },
   ];
+
+  /*
+   * event listener for change of selection on
+   * example selector
+   * */
+  var onChangeSelection = function () {
+    exampleSelector.change(function (v) {
+      var selection = $(this);
+      var path = selection.find(":selected").val();
+      loadExample(path);
+    });
+  };
+
+  /**
+   * Loads and displays the example identified by the given name
+   */
+  var loadExample = function (example) {
+    $.ajax({
+      url: "examples/" + example,
+      dataType: "text",
+    })
+      .done(function (code) {
+        var aceId = $("#ace-editor").attr("id");
+        var editor = ace.edit(aceId);
+        editor.getSession().setValue(code);
+      })
+      .fail(function () {
+        $("#result").html("Sorry, I could not retrieve the example!");
+      });
+  };
+
+  var loadSelection = function () {
+    var example = exampleSelector.find(":selected").val();
+    loadExample(example);
+  };
+
+  var init = function () {
+    createEditor();
+    selections.forEach(function (v) {
+      exampleSelector.append(new Option(v.name, v.path));
+    });
+    loadSelection();
+    onChangeSelection();
+  };
+
+  init();
 });
---
+++ tests/README.md
@@ -0,0 +1,12 @@
+# Tests
+
+Tests are here. The package.json and so forth is here not above, to cleanly
+divide the core lib and NPM.
+
+These tests use mocha currently.
+
+To run:
+
+- `npm install`
+- `npm test`
+- `yarn test`
